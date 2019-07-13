72098964fae8323a5f40b20bbfbbb4564a3f9e59
tkvsoft <52506746+tkvsoft@users.noreply.github.com>
2019-07-13 18:07:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>AJAX LOGIN</title>
+<title>AJAX WITH PHP LOGIN</title>
 <link rel="shortcut icon" type="image/x-icon" href="">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="description" content="" />
 <meta name="keywords" content="">
 <meta name="robots" content="noindex" >
-<script src="jquery-3.3.1.js" language="javascript"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.js" language="javascript"></script>
 <script type="text/javascript">
 $(document).ready(function() {
     $('#loginform').submit(function(e) {
@@ -20,7 +20,8 @@ $(document).ready(function() {
             {
                 var jsonData = JSON.parse(response);
  
-             
+                // user is logged in successfully in the back-end
+                // let's redirect
                 if (jsonData.success == "1")
                 {
                     location.href = 'home.php?name='+jsonData.name;
@@ -36,6 +37,16 @@ $(document).ready(function() {
 </script>
 </head>
 <body>
+
+<form id="loginform" method="post">
+    <div style="padding:20px;border:solid 1px #666666;">
+        <strong>Username:</strong>
+        <input type="text" name="username" id="username" />
+        <strong>Password:</strong>
+        <input type="password" name="password" id="password" />    
+        <input type="submit"  value="LogIn" />
+    </div>
+</form>
 
 </body>
 </html>
